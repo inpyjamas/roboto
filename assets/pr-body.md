829eed01ce3a4c70b6ef85b2e9351e7ca23f10d6
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-09-04 12:26:47 +0200

    init first working setup

---
+++ assets/pr-body.md
@@ -0,0 +1,11 @@
+Hey ho. It's me your friendly neighborhood Roboto and I'd like to contribute some files to your new repo.
+That's why I created a file to under your .github folder. So we have some content we can work with.
+
+This PR will contain some files you will need in any repository. Currently its these:
+
+- `.github/renovate.json`
+- `.github/CODEOWNERS`
+
+It would be great to merge these so renovate-bot can start to keep your dependencies up to date and new PRs are assigned to you as a codeowner.
+
+To learn more about [me](#), [renovate-bot](https://renovate.whitesourcesoftware.com/) and [codeowners](https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/about-code-owners) follow the links.
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,53 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+// For more information on building apps:
+// https://probot.github.io/docs/
 
+// To get your app running against GitHub, see:
+// https://probot.github.io/docs/development/
+import { Application } from "probot";
+import { composeCreatePullRequest } from "octokit-plugin-create-pull-request";
+import fs from "fs";
+import path from "path";
+const prBody = fs.readFileSync(
+  path.resolve(__dirname, "../assets/pr-body.md"),
+  "utf8"
+);
+const renovateJson = fs.readFileSync(
+  path.resolve(__dirname, "../assets/renovate.json"),
+  "utf8"
+);
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on("repository.created", async (context) => {
+    const {
+      payload: { repository },
+      log,
+    } = context;
+    const configResponse = await context.github.repos.createOrUpdateFileContents(
+      {
+        owner: repository.owner.login,
+        repo: repository.name,
+        path: ".github/inpyjamas-roboto.config.yml",
+        message: "init roboto config",
+        content: Buffer.from("# hello world").toString("base64"),
+      }
+    );
+    if (configResponse === null) return;
+    log(configResponse);
+    const response = await composeCreatePullRequest(context.github, {
+      owner: repository.owner.login,
+      repo: repository.name,
+      title: "inpyjamas-roboto default configs",
+      body: prBody,
+      head: `chore/inpyjamas-roboto-onboard`,
+      changes: {
+        commit: "inpyjamas-roboto onboarding",
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+        files: {
+          ".github/renovate.json": renovateJson,
+          ".github/CODEOWNERS": `* @${repository.owner.login}`,
+        },
+      },
+    });
+    if (response === null) return;
+    log(response);
+  });
+};
